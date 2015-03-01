c70729a2265f36cf2f793008964995c92af7d674
Anthony Nardi <anthonygt@gmail.com>
2015-02-28 19:35:12 -0500

    Seperate process creates webm blob, use window.createObjectUrl(webmVideo)

--- process.js
+++ process.js
@@ -3,6 +3,8 @@ console.log('New nwjs instance started.');
 var app = require('http').createServer(handler)
 var io = require('socket.io')(app);
 var fs = require('fs');
+var bufferToWebp = require('./lib/bufferToWebp');
+var webpToWebm   = require('./lib/webpToWebm');
 
 var framesReceived = 0;
 var frameBuffer    = [];
@@ -16,11 +18,12 @@ function handler (req, res) {
 }
 
 io.on('connection', function (socket) {
+
   
   socket.on('recordFrame', function (data) {
-  	if (!firstFrameTimestamp) {
-  		firstFrameTimestamp = Date.now();
-  	}
+    if (!firstFrameTimestamp) {
+      firstFrameTimestamp = Date.now();
+    }
     framesReceived++;
     frameBuffer.push(data);
     console.log('GOT FRAME!');
@@ -28,10 +31,48 @@ io.on('connection', function (socket) {
   });
 
   socket.on('stopRecording', function () {
-  	console.log(framesReceived + ' frames received.');
-  	console.log(frameBuffer[0]);
-  	console.log((Date.now() - firstFrameTimestamp) / 1000);
-  	buf = frameBuffer[0];
+
+    console.log(framesReceived + ' frames received.');
+    console.log(frameBuffer[0]);
+    console.log((Date.now() - firstFrameTimestamp) / 1000);
+    
+    var webpBuffer  = new bufferToWebp();
+    var webpEncodedBuffer = webpBuffer.encode(frameBuffer)
+    var webmBuffer  = new webpToWebm(webpBuffer.encode(frameBuffer)); 
+
+    var webmVideo = webmBuffer.encode();
+
+    window.webmVideo = webmVideo;
+
+    console.log('VIDEO GENERATED.');
+
+    window.webkitStorageInfo.requestQuota(PERSISTENT, webmVideo.size, function (grantedBytes) {
+  
+      window.webkitRequestFileSystem(window.PERSISTENT, webmVideo.size, function (fileSys) {
+        
+        console.log('success');
+        
+        window.fileSys = fileSys;
+        
+        fileSys.root.getFile('log.txt', {create: true, exclusive: false}, function(fileEntry) {
+          window.fileEntry = fileEntry;
+          // fileEntry.isFile === true
+          // fileEntry.name == 'log.txt'
+          // fileEntry.fullPath == '/log.txt'
+
+        }, function (e) {console.log(e)});
+
+      }, function () {
+        console.log('ERROR FUCK');
+      });
+      
+    }, function (e) {
+      console.log('Error', e);
+    });
+
+
+
   })
 
 });
+
