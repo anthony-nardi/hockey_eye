3a0720fd40bb259943870b2c2272fb6821d80939
Anthony Nardi <anthonygt@gmail.com>
2015-03-26 17:07:58 -0400

    How do we call that function?

--- lib/src/printBuffer.cpp
+++ lib/src/printBuffer.cpp
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <cstdint>
 
 #include "./libwebp/webp/encode.h"
 #include <node.h>
+
+
 
 using namespace v8;
 
@@ -14,12 +17,71 @@ void Uint8ToRgba(const FunctionCallbackInfo<Value>& args) {
   // Enter the context scope we just created.
   HandleScope scope(isolate);
 
-  Local<Object> varName = args[0]->ToObject();
 
-  //printf("varName.length%d", *varName);
-  //
+
+  // Local<Object> bufferObj = args[0]->ToObject();
   
-  args.GetReturnValue().Set(varName);
+
+
+  // char* bufferData = Buffer::Data(bufferObj);
+  // size_t bufferLength = Buffer::Length(bufferObj);
+  // Local<Object> varName = args[0]->ToObject();
+
+  
+
+  // Local<ArrayBuffer> myBuff = ArrayBuffer::New (*isolate, args[0], 100);
+  // Local<TypedArray> typedArray = v8::Handle<v8::TypedArray>::Cast(args[0]);
+
+  // Local<Uint8Array> uint8Array = v8::Handle<v8::Uint8Array>::Cast(typedArray);
+
+  // std::uint8_t *buffer = arrayBuf;
+
+  // string s = std::type_info(args[0]).name();
+  // printf('%s', s);
+  //printf("varName.length%d", *varName);
+  
+// v8::Handle<v8::Array> buffer = v8::Handle<v8::Array>::Cast(arg);
+
+  std::uint8_t* retb = NULL;
+  // std::uint8_t *buffer =  v8::Handle<v8::Array>::Cast(*args[0]);
+  // printf("size: %d\n", (int)sizeGetIndexedPropertiesPixelData);
+  // free(buffer);
+  // free(retb);
+  // return 0;
+  // 
+  // 
+  
+  // uint8_t *buffer = arrayBuf->GetIndexedPropertiesPixelData();
+
+
+
+
+  
+
+  // buffer = (uint8_t*)malloc(size);
+
+  // buffer = arrayBuf;
+
+  Local<ArrayBuffer> arrayBuf = v8::Handle<v8::ArrayBuffer>::Cast(args[0]);
+  // Local<ArrayBufferView> arrayBufView = v8::Handle<v8::ArrayBufferView>::Cast(arrayBuf);
+  // Local<TypedArray> typedArray = v8::Handle<v8::TypedArray>::Cast(args[0]);
+  // Local<Uint8Array> uint8Array = v8::Handle<v8::Uint8Array>::Cast(args[0]);
+
+  // size_t size = args[0]->ByteLength();
+	// size_t size = uint8Array->ByteLength();
+  // Local<Number> num = Number::New(isolate, size);
+  // printf("size%d", size);
+  ArrayBuffer::Contents mycontent = arrayBuf->Externalize();
+  
+  std::uint8_t *buffer = NULL;
+  
+  buffer = (uint8_t*)mycontent.Data();
+
+  size_t size2 = WebPEncodeRGBA(buffer, 1920, 1080, 1920, 100, &retb);
+ 
+ args.GetReturnValue().Set(arrayBuf); 
+ // args.GetReturnValue().Set(uint8Array->IsUint8ClampedArray());
+  // args.GetReturnValue().Set(num);
 
 }
 
