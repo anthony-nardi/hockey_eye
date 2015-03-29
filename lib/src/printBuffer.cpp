18a2a84ac5160c470719c8b9319627053371fcce
Anthony Nardi <anthonygt@gmail.com>
2015-03-29 09:59:17 -0400

    Yay, it possibly works

--- lib/src/printBuffer.cpp
+++ lib/src/printBuffer.cpp
@@ -1,8 +1,26 @@
+// BINDINGS
+// 
+// RELATIVE TO PROJECT : "C:\\Users\\tony\\hockey_eye\\lib\\src\\libwebp\\src\\.libs\\libwebp.dll.a"
+// CYGWIN              : "C:\\cygwin64\\usr\\local\\lib\\libwebp.dll.a"
+// VISUAL C++          : "C:\\Users\\tony\\hockey_eye\\lib\\src\\libwebp\\output\\release-static\\x64\\lib"
+
+//
+        // "C:\\Users\\tony\\hockey_eye\\lib\\src\\libwebp-0.4.3\\output\\release-static\\x64\\lib\\libwebpdecoder",
+        // "C:\\Users\\tony\\hockey_eye\\lib\\src\\libwebp-0.4.3\\output\\release-static\\x64\\obj\\enc\\webpenc.obj"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <cstdint>
+#include <vector>
+  
+// RELATIVE TO PROJECT
+#include "libwebp-0.4.3/src/webp/encode.h"
+#include "libwebp-0.4.3/src/webp/decode.h"
 
-#include "./libwebp/webp/encode.h"
+// CYGWIN
+// #include "C:/cygwin64/usr/local/include/webp/encode.h"
+// #include "C:/cygwin64/usr/local/include/webp/decode.h"
+
 #include <node.h>
 
 
@@ -62,26 +80,43 @@ void Uint8ToRgba(const FunctionCallbackInfo<Value>& args) {
 
   // buffer = arrayBuf;
 
-  Local<ArrayBuffer> arrayBuf = v8::Handle<v8::ArrayBuffer>::Cast(args[0]);
+  // Local<ArrayBuffer> arrayBuf = v8::Handle<v8::ArrayBuffer>::Cast(args[0]);
+  Local<Uint8Array> uint8Array = v8::Handle<v8::Uint8Array>::Cast(args[0]);
+
   // Local<ArrayBufferView> arrayBufView = v8::Handle<v8::ArrayBufferView>::Cast(arrayBuf);
   // Local<TypedArray> typedArray = v8::Handle<v8::TypedArray>::Cast(args[0]);
   // Local<Uint8Array> uint8Array = v8::Handle<v8::Uint8Array>::Cast(args[0]);
 
   // size_t size = args[0]->ByteLength();
-	// size_t size = uint8Array->ByteLength();
+  // size_t size = uint8Array->ByteLength();
   // Local<Number> num = Number::New(isolate, size);
   // printf("size%d", size);
-  ArrayBuffer::Contents mycontent = arrayBuf->Externalize();
+  // ArrayBuffer::Contents mycontent = arrayBuf->Externalize();
   
-  std::uint8_t *buffer = NULL;
+  // std::uint8_t *buffer = NULL;
   
-  buffer = (uint8_t*)mycontent.Data();
+  // buffer = (uint8_t*)mycontent.Data();
 
-  size_t size2 = WebPEncodeRGBA(buffer, 1920, 1080, 1920, 100, &retb);
- 
- args.GetReturnValue().Set(arrayBuf); 
- // args.GetReturnValue().Set(uint8Array->IsUint8ClampedArray());
-  // args.GetReturnValue().Set(num);
+  // uint8_t myimg[8] = {255,255,255,1,122,122,111,1};
+  std::vector<uint8_t> cppArrayBuf;
+  
+  static int const n_items = uint8Array->ByteLength();
+
+  for (int i = 0; i < n_items; i ++) {
+    cppArrayBuf.push_back(uint8Array->Get(i)->Uint32Value());
+  }
+
+  std::uint8_t *buffer = NULL;
+
+  buffer = (uint8_t*)cppArrayBuf.data();
+
+
+  size_t size2 = WebPEncodeRGBA(buffer, 1920, 1080, 1920, 50, &retb);
+
+  Local<Number> num = Number::New(isolate, size2);
+
+  args.GetReturnValue().Set(num); 
+
 
 }
 
@@ -89,4 +124,4 @@ void init(Handle<Object> exports) {
   NODE_SET_METHOD(exports, "uint8ToRgba", Uint8ToRgba);
 }
 
-NODE_MODULE(webpEncoder, init)
+NODE_MODULE(printBuffer, init)
